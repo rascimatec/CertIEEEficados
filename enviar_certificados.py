cd84f6023acbe073d7e6146b889fe3e3c7cd4b98
Felipe Mohr <felipe18mohr@gmail.com>
2020-10-20 02:16:12 -0300

    enviando diversos certificados por email de uma vez

--- enviar_certificados.py
+++ enviar_certificados.py
@@ -2,7 +2,7 @@ import smtplib
 import pandas as pd
 from email.message import EmailMessage
 
-df = pd.read_csv('recursos/nomes.csv')
+certificados = ['abertura', 'analise_de_dados']
 
 from_email = input("Endereço de email: ")
 password = input("Digite a senha: ")
@@ -14,19 +14,23 @@ server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login(from_email, password)
 print("Login sucedido")
 
-for lin, col in df.iterrows():
+for certificado in certificados:
+  print("ENVIANDO " + certificado)
+  df = pd.read_csv('_recursos/csv/'+certificado+'.csv')
+  for lin, col in df.iterrows():
+    
+    msg = EmailMessage()
+    msg['From'] = from_email
+    msg['Subject'] = titulo
+    msg['To'] = col['Endereço de e-mail']
+    msg.set_content(texto)
+
+    file_name = col['Nome Completo']+'.pdf'
+    with open('_certificados/'+certificado+'/'+file_name, 'rb') as f:
+      file_data = f.read()
+
+    msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+    server.send_message(msg)
+    print("  -> Email enviado para " + col['Nome Completo'])
   
-  msg = EmailMessage()
-  msg['From'] = from_email
-  msg['Subject'] = titulo
-  msg['To'] = col['Email']
-  msg.set_content(texto)
-
-  file_name = col['Nome']+'.pdf'
-  with open('certificados/'+file_name, 'rb') as f:
-    file_data = f.read()
-
-  msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
-
-  server.send_message(msg)
-  print("Email enviado para " + col['Nome'])
--- preencher_certificados.py
+++ preencher_certificados.py
@@ -3,11 +3,22 @@ import pandas as pd
 import textwrap
 import os.path
 
-certificados = ['arduino_basico', 'arduino_avançado']
-coluna_nomes = 'Nome'
-pasta_dados = 'recursos/csv'
-pasta_modelos = 'recursos/modelos'
-font = ImageFont.truetype(font="recursos/fonte.ttf", size=60)
+certificados = ['abertura', 'analise_de_dados']
+
+y1 = 0.41
+y2 = 0.375
+width = 30
+font_size = 85
+espacamento = 4
+
+coluna_nomes = 'Nome Completo'
+pasta_dados = '_recursos/csv'
+pasta_modelos = '_recursos/modelos'
+pasta_certificados = '_certificados'
+font = ImageFont.truetype(font="_recursos/fonte.ttf", size=font_size)
+
+if not os.path.isdir(pasta_certificados):
+  os.mkdir('./'+pasta_certificados)
 
 dados = list()
 modelos = list()
@@ -15,7 +26,7 @@ pastas = list()
 for wk in certificados:
   dados.append(wk+'.csv')
   modelos.append(wk+'.png')
-  pastas.append(wk)
+  pastas.append(pasta_certificados+'/'+wk)
 
 for pasta in pastas:
   if not os.path.isdir(pasta):
@@ -30,12 +41,16 @@ for i, wk in enumerate(dados):
   for lin, col in dados.iterrows():
     certificado = img.copy()
     text = col[coluna_nomes]
-    text_wrap = textwrap.wrap(text, width=25)
+    text_wrap = textwrap.wrap(text, width=width)
     draw = ImageDraw.Draw(certificado)
-    current_h = 0.35*H if (len(text_wrap) < 2) else 0.32*H
+    current_h = y1*H if (len(text_wrap) < 2) else y2*H
     for line in text_wrap:
       w, h = draw.textsize(line, font=font)
-      draw.text(xy=((W-w)/2, (current_h)), text=line, fill=(0,0,0), font=font)
-      current_h += h + 4
+      draw.text(xy=((W-w)/2, (current_h)), text=line, fill=(255,255,255), font=font)
+      current_h += h + espacamento
     certificado_pdf = certificado.convert('RGB')
     certificado_pdf.save(pastas[i]+f'/{text}.pdf')
+
+  print(wk[:-4]+ " finalizado...")
+
+print("Todos os certificados foram gerados com sucesso!")
