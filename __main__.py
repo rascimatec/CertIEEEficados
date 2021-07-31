43afdad59950ea3ba423528dc9fc7575659dca50
Felipe Mohr <felipe18mohr@gmail.com>
2021-07-31 03:15:54 -0300

    create class send

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from preencher_certificados import PreencherCertificados
+from enviar_certificados import EnviarCertificados
+
+if __name__ == "__main__":
+  pass
--- enviar_certificados.py
+++ enviar_certificados.py
@@ -2,35 +2,39 @@ import smtplib
 import pandas as pd
 from email.message import EmailMessage
 
-certificados = ['abertura', 'analise_de_dados']
+class EnviarCertificado:
+  def __init__(self):
+    self.certificados = ['abertura', 'analise_de_dados']
 
-from_email = input("Endereço de email: ")
-password = input("Digite a senha: ")
+  def setEmail(self):
+    self.from_email = input("Endereço de email: ")
+    self.password = input("Digite a senha: ")
 
-titulo = 'Teste de email com anexo em Python'
-texto = 'Segue o certificado em anexo'
+    self.titulo = 'Teste de email com anexo em Python'
+    self.texto = 'Segue o certificado em anexo'
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login(from_email, password)
-print("Login sucedido")
+    self.server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    self.server.login(self.from_email, self.password)
+    print("Login sucedido")
 
-for certificado in certificados:
-  print("ENVIANDO " + certificado)
-  df = pd.read_csv('_recursos/csv/'+certificado+'.csv')
-  for lin, col in df.iterrows():
+def enviarCertificados(self):
+  for certificado in self.certificados:
+    print("ENVIANDO " + certificado)
+    df = pd.read_csv('_recursos/csv/'+certificado+'.csv')
+    for lin, col in df.iterrows():
+      
+      msg = EmailMessage()
+      msg['From'] = self.from_email
+      msg['Subject'] = self.titulo
+      msg['To'] = col['Endereço de e-mail']
+      msg.set_content(self.texto)
+
+      file_name = col['Nome Completo']+'.pdf'
+      with open('_certificados/'+certificado+'/'+file_name, 'rb') as f:
+        file_data = f.read()
+
+      msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+      self.server.send_message(msg)
+      print("  -> Email enviado para " + col['Nome Completo'])
     
-    msg = EmailMessage()
-    msg['From'] = from_email
-    msg['Subject'] = titulo
-    msg['To'] = col['Endereço de e-mail']
-    msg.set_content(texto)
-
-    file_name = col['Nome Completo']+'.pdf'
-    with open('_certificados/'+certificado+'/'+file_name, 'rb') as f:
-      file_data = f.read()
-
-    msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
-
-    server.send_message(msg)
-    print("  -> Email enviado para " + col['Nome Completo'])
-  
