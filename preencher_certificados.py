67b16fb015fd5f1e502c8e2391d0d5820ef64ec0
Felipe Mohr <felipe18mohr@gmail.com>
2021-08-01 21:40:00 -0300

    Update grafic interface and fix errors

--- preencher_certificados.py
+++ preencher_certificados.py
@@ -5,8 +5,6 @@ import textwrap
 import os.path
 
 # TO-DO:
-# Adicionar arquivo de fonte à interface
-# rever a coluna col_lines
 # Atualizar a imagem do certificado automaticamente
 
 class PreencherCertificados:
@@ -15,33 +13,32 @@ class PreencherCertificados:
     
     sg.theme('DarkAmber')
 
-    self.__col_browse = [[sg.Text('Pasta de Dados (.CSV)', size=(20,0)), sg.Input(size=(40,0), key='csv_folder'), sg.FolderBrowse()],
-                         [sg.Text('Pasta de Modelos', size=(20,0)), sg.Input(size=(40,0), key='__models_folder'), sg.FolderBrowse()],
-                         [sg.Text('Pasta de Certificados', size=(20,0)), sg.Input(size=(40,0), key='destination_folder'), sg.FolderBrowse()],
+    self.__col_browse = [[sg.Text('Pasta de Dados (.CSV)', size=(20,0)), sg.Input(size=(40,0), key='csv_folder', default_text="./_recursos/csv"), sg.FolderBrowse()],
+                         [sg.Text('Pasta de Modelos', size=(20,0)), sg.Input(size=(40,0), key='models_folder', default_text="./_recursos/modelos"), sg.FolderBrowse()],
+                         [sg.Text('Pasta de Certificados', size=(20,0)), sg.Input(size=(40,0), key='destination_folder', default_text="./certificados"), sg.FolderBrowse()],
                          [sg.Text('Altura da linha (única / múltiplas)', size=(28,0)), sg.Slider(range=(0,1000), default_value=500, orientation='h', size=(17,12), key="y1"),
                           sg.Slider(range=(0,1000), default_value=500, orientation='h', size=(17,12), key="y2")]]
-    self.__col_lines = [sg.Text('Altura da linha (única)', size=(22,0)), sg.Slider(range=(0,1000), default_value=500, orientation='h', size=(15,12), key="y1"),
-                        sg.Text('Altura da linha (múltiplas)', size=(22,0)), sg.Slider(range=(0,1000), default_value=500, orientation='h', size=(15,12), key="y2")]
-    self.__col_font = [[sg.Text('Comprimento', size=(15,0)), sg.Input(size=(10, 0), key="width")],
-                       [sg.Text('Tamanho da fonte', size=(15,0)), sg.Input(size=(10, 0), key="font_size")],
-                       [sg.Text('Espaçamento',size=(15,0)), sg.Input(size=(10,0), key="espacamento")]]
+    self.__col_font = [[sg.Text('Comprimento', size=(15,0)), sg.Input(size=(6, 0), key="width")],
+                       [sg.Text('Tamanho da fonte', size=(15,0)), sg.Input(size=(6, 0), key="font_size")],
+                       [sg.Text('Espaçamento',size=(15,0)), sg.Input(size=(6  ,0), key="espacamento")]]
     self.__col_font_color = [[sg.Text('Red', size=(5,0)), sg.Spin([rc for rc in range(0, 255)], key="red", size=(4,0))],
                              [sg.Text('Green', size=(5,0)), sg.Spin([gc for gc in range(0, 255)], key="green", size=(4,0))],
                              [sg.Text('Blue', size=(5,0)), sg.Spin([bc for bc in range(0, 255)], key="blue", size=(4,0))]]
-    
-    self.__col_total = [[sg.Column(self.__col_browse)], [sg.Column(self.__col_font), sg.Column(self.__col_font_color)]]
-# [sg.Column(self.__col_lines),
-    self.__layout = [[sg.Image(r'wallpaper_ieee.png'), sg.Column(self.__col_total)],
-                     [sg.Button('Anterior', size=(12,0), key="prev"), sg.Button('Proximo', size=(12,0), key="next"), 
-                      sg.Text(size=(60,0)), sg.Cancel(), sg.OK()]]
-    
-    self.coluna_nomes = 'Nome Completo'
+    self.__col_name = [[sg.Text('Coluna de Nomes', size=(15,0)), sg.Input(size=(15,0), key="names_col", default_text="Nome Completo")],
+                       [sg.Text('Fonte', size=(5,0)), sg.Input(size=(15,0), key='font', default_text="./recursos/fonte.ttf"), sg.FileBrowse(size=(4,0))],
+                       [sg.Cancel(), sg.Button('Update'), sg.OK(size=(5,0))]]
 
+    self.__col_total = [[sg.Column(self.__col_browse)], [sg.Column(self.__col_font), sg.Column(self.__col_font_color), sg.Column(self.__col_name)]]
+
+    self.__layout = [[sg.Image(r'wallpaper_ieee.png'), sg.Column(self.__col_total)],
+                     [sg.Button('<<', size=(3,0), key="prev_model"), sg.Button('>>', size=(3,0), key="next_model"), sg.Text(size=(5,0)), sg.Button('<', size=(3,0), key="prev"), sg.Button('>', size=(3,0), key="next")]]
+    
     self.__data = list()
     self.__models = list()
     self.__folders = list()
 
     self.__cont = 0
+    self.__cont_model = 0
 
 
   def initInterface(self):
@@ -50,10 +47,14 @@ class PreencherCertificados:
       self.event, self.values = self.window.read()
       if self.event in (sg.WIN_CLOSED, 'Cancel'):
         break
-      elif self.event == 'Anterior' and self.__cont > 0:
+      elif self.event == '<' and self.__cont > 0:
         self.__cont -= 1
-      elif self.event == 'Proximo' and self.__cont < 10:
+      elif self.event == '>' and self.__cont < 10:
         self.__cont += 1
+      elif self.event == '<<' and self.__cont_model > 0:
+          self.__cont_model -= 1
+      elif self.event == '>>' and self.__cont_model < 10:
+        self.__cont_model += 1
       elif self.event == 'OK':
         self.getData()
         self.generateCertificates()
@@ -63,14 +64,14 @@ class PreencherCertificados:
 
   def getData(self):
 
-    self.__certificates = [os.path.splitext(filename)[0] for filename in os.listdir(self.values['destination_folder'])]
     self.__font = ImageFont.truetype(font="_recursos/fonte.ttf", size=int(self.values['font_size']))
 
     if not os.path.isdir(self.values['destination_folder']):
       os.mkdir('./'+self.values['destination_folder'])
+    self.__certificates = [os.path.splitext(filename)[0] for filename in os.listdir(self.values['destination_folder'])]
 
-    self.__certificates = ['abertura']
-    for wk in self.__certificates:
+    self.__wks = [os.path.splitext(filename)[0] for filename in os.listdir(self.values['csv_folder'])]
+    for wk in self.__wks:
       self.__data.append(wk+'.csv')
       self.__models.append(wk+'.png')
       self.__folders.append(self.values['destination_folder']+'/'+wk)
@@ -78,7 +79,7 @@ class PreencherCertificados:
   def writeCertificate(self, img, col):
     certificado = img.copy()
     W, H = certificado.size
-    text = col[self.coluna_nomes]
+    text = col[self.values['names_col']]
     text_wrap = textwrap.wrap(text, width=int(self.values['width']))
     draw = ImageDraw.Draw(certificado)
     current_h = (int(self.values['y1'])/1000)*H if (len(text_wrap) < 2) else (int(self.values['y2'])/1000)*H
@@ -98,7 +99,7 @@ class PreencherCertificados:
     for i, wk in enumerate(self.__data):
 
       self.__data = pd.read_csv(self.values['csv_folder']+'/'+wk)
-      img = Image.open(self.values['__models_folder']+'/'+self.__models[i])
+      img = Image.open(self.values['models_folder']+'/'+self.__models[i])
 
       for _, col in self.__data.iterrows():
         certificado, name = self.writeCertificate(img, col)
